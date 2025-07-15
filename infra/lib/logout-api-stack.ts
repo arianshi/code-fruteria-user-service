73c8c471978d3c754cd7f37274955ef373565b68
arianshi <arian.shi@outlook.com>
2025-07-15 16:17:15 +0800

    fix: token issues

--- infra/lib/logout-api-stack.ts
+++ infra/lib/logout-api-stack.ts
@@ -3,6 +3,7 @@ import { Construct } from 'constructs'
 import { IRestApi } from 'aws-cdk-lib/aws-apigateway'
 import { Topic } from 'aws-cdk-lib/aws-sns'
 import { RestEndpoint } from './rest-endpoint'
+import { Secret } from 'aws-cdk-lib/aws-secretsmanager'
 
 interface LogoutApiStackProps extends NestedStackProps {
   api: IRestApi
@@ -14,6 +15,15 @@ export class LogoutApiStack extends NestedStack {
   constructor(scope: Construct, id: string, props: LogoutApiStackProps) {
     super(scope, id, props)
 
+
+    const JWT_SECRET = Secret.fromSecretNameV2(
+          this,
+          'JwtSecretKey',
+          `/${props.serviceStage}/jwt-secret/key`
+        )
+          .secretValueFromJson('key')
+          .unsafeUnwrap()
+          
     new RestEndpoint(this, 'LogoutEndpoint', {
       api: props.api,
       entry: '../app/lambda/logout.lambda.ts',
@@ -22,7 +32,8 @@ export class LogoutApiStack extends NestedStack {
       timeout: Duration.seconds(10),
       memorySize: 128,
       environment: {
-        STAGE: props.serviceStage
+        STAGE: props.serviceStage,
+        JWT_SECRET: JWT_SECRET,
       },
       alarmsTopic: props.alarmsTopic,
     })
